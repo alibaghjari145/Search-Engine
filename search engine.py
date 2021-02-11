65cd131c27d3e43e1d34d8f36da173f94e74410a
ali baghjari <70961525+alibaghjari145@users.noreply.github.com>
2021-02-11 05:07:50 -0500

    Add files via upload

---
+++ search engine.py
@@ -0,0 +1,13 @@
+
+text = input()
+word = input()
+
+def search(x,y):
+        if y in x:
+            return "Word found"
+        else:
+            return "Word not found"
+        w+=1
+
+
+print(search(text, word))
